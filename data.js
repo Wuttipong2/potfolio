ee12654c9b5220c4c92a2dfadd0750421bfcda78
wuttipong phongam <614259046@webmail.npru.ac.th>
2020-10-06 12:05:28 +0700

    add work

---
+++ data.js
@@ -0,0 +1,53 @@
+const data = {
+    "fullname" : "Mr. Wuttipong  Phongam",
+    "nickname" : "All New",
+    "motto" : "ทำดี ย่อมได้ดี ทำอัปปรีย์ มันจะได้ดีได้ยังไง.",
+    "work" : "Student at Software Engineering Program in Nakhon Pathom Rajabhat University",
+    "aboutme" : {
+        "name" : "Mr.Wuttipong Phongam",
+        "nickname" : "New",
+        "sex" : "Male",
+        "education" : "Sakaka Thiam Wittayakhom School",
+        "date" : "April 2, 2000",
+        "age" : "20 years",
+        "nationality" : "Thai",
+        "race" : "Thai",
+        "religion" : "Buddhism",
+        "bloodgroup" : "O",
+        "address" : "69 Moo 3, Nong Din Daeng Subdistrict, Mueang District, Nakhon Pathom Province 73000",
+        "phone" : "0927312299",
+        "email" : "614259046@webmail.npru.ac.th",
+        "characteristic" : "Talk openly",
+        "hobbies" : "Watching movies, selling stuff, sleeping, driving, playing, going out.",
+        "sport" : "Futsal",
+        "pets" : "cat",
+        "attitude" : "Positive",
+        "motto" : "ทำดี ย่อมได้ดี ทำอัปปรีย์ มันจะได้ดีได้ยังไง."
+    },
+    "education" : {
+        "education1" : "Wat Nong Suea School",
+        "education2" : "Sakaka Thiam Wittayakhom School",
+        "education3" : "Sakaka Thiam Wittayakhom School",
+        "education4" : "Nakhon Pathom Rajabhat University"
+    },
+    "experience" : {
+        "experience1" : "none"
+    },
+    "skill" : {
+        "html" : "Level-Basic",
+        "css" : "Level-Basic",
+        "php" : "Level-Basic",
+        "java" : "Level-Basic",
+        "javascript" : "Level-Basic",
+        "sql" : "Level-Basic",
+        "json" : "Level-Basic"
+    },
+    "contact" : {
+        "fb" : "Wuttipong",
+        "ig" : "n.new",
+        "line" : "0927312299",
+        "email" : "614259046@webmail.npru.ac.th",
+        "phone" : "0927312299"
+    }
+};
+module.exports = data;
